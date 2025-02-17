6b3aa06bbca838afd410349d39b7f74d28e87bd5
davimussato <164518736+davimussato@users.noreply.github.com>
2025-02-17 10:12:05 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ print("2- listar restaurante")
 print("3- ativar restaurante ")
 print("4- sair")
 
-opcao_escolida = input ("escolha uma opcao")
-print (f"voce escolheu a opcao_escolida"{opcao_escolida})
+opcao_escolida = int (input(opcao_escolida))
 
-
+if opcao_escolida == 1:
+    print('cadastrar restaurante')
+elif opcao_escolida == 2:
+    print ('lista restaurante')
+elif (opcao_escolida) == 3:
+    print ('ativar restaurante')
+else:
+    print ('encerrando programa')
